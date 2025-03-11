e72ec75a4f5b2a9943ee0386f382b9d400df9dd3
rciwilmer <rciugelpataz@gmail.com>
2025-03-10 22:10:09 -0500

    ProyectoWeb_01

---
+++ script.js
@@ -0,0 +1,4 @@
+// Este archivo está listo para ser utilizado con cualquier funcionalidad JavaScript adicional
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("La página está completamente cargada.");
+});
